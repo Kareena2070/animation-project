55cc8065354c871da133ce1148e3309bb5bd608f
Kareena2070 <kareenayadav1088@gmail.com>
2025-10-30 23:41:44 +0530

    learn scrollTriger-trigger, scoller, markers and scrub apply on divs

---
+++ animation-basic2/script.js
@@ -0,0 +1,59 @@
+gsap.to(".page1 #box", {
+    scale:1,
+    delay:1,
+    duration:2,
+    rotation:360,
+    scrollTrigger: {
+        trigger: ".page1 #box",
+        scroller: "body",
+        start: "top 60%",
+        end: "top 10%",
+        scrub: 2,
+        markers: true,
+        // pin: true,
+    }
+})
+
+gsap.to(".page2 h1", {
+    opacity:0,
+    x:500,
+    duration:2,
+    // scrollTrigger: ".page2 #box"
+    scrollTrigger: {
+        trigger: ".page2 h1",
+        scroller: "body",
+        start: "top 50%",
+        end: "top 10%",
+        markers: true,
+        scrub: 2,
+    }
+})
+
+gsap.from (".page2 h2", {
+    opacity:0,
+    x:-500,
+    duration:2,
+    scrollTrigger: {
+        trigger: ".page2 h2",
+        scroller: "body",
+        start: "top 50%",
+        // end: "top 10%",
+        markers: true,
+        scrub: 2,
+    }
+})
+
+gsap.from(".page3 #box", {
+    rotation:360,
+    scale:1,
+    duration:2,
+    rotation:720,
+    scrollTrigger: {
+        trigger: ".page3 #box",
+        scroller: "body",
+        start: "top 60%",
+        end: "top 10%",
+        scrub: 2,
+        markers: true,
+    }
+})
