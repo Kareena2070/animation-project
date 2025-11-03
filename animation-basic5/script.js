4a7b505c060d48d2b397ce74ff2d196fb6103823
Kareena2070 <kareenayadav1088@gmail.com>
2025-11-03 14:44:27 +0530

    work on gsap timeline, in timeline I use pause, play and reverse

--- animation-basic5/script.js
+++ animation-basic5/script.js
@@ -1,7 +1,31 @@
-var tl = gsap.timeline()
+// var tl = gsap.timeline({paused:true}); we can do this way also and then use tl.play() to play the animation on some event like click
+var tl = gsap.timeline();
+
+let menu = document.querySelector("#nav i")
+let cross = document.querySelector("#full i")
 
 tl.to("#full", {
     right: 0,
-    duration: 1.5,
-    // ease: "power2.out"
+    duration: 0.5,
+})
+
+tl.from("#full h4", {
+   x:150,
+   duration:0.6,
+   stagger:0.3,
+    opacity:0
+})
+
+tl.from("#full i", {
+    opacity:0,
+})
+
+tl.pause();
+
+menu.addEventListener("click", function(){
+    tl.play();
+})
+
+cross.addEventListener("click", function(){
+    tl.reverse();
 })
